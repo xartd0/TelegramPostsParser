7907ca4137ae81961c2753b204da130a4bb88a96
Xartd0 <43171120+xartd0@users.noreply.github.com>
2023-12-02 02:47:35 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-PASSWORD = '' # Указываем ваш пароль, если есть пароль на 2fa
-PHONE = ''
-
-API_ID = ''
-API_HASH = ''
-
-CHANNEL_TO_SEND = 
-CHANNEL_TO_TRACK = 
+ PASSWORD = '' #Your password from 2fa
+ PHONE = '' #Your phone number
+ 
+ API_ID = '' #from https://my.telegram.org/auth
+ API_HASH = '' #from https://my.telegram.org/auth
+ 
+ CHANNEL_TO_SEND = #The ID of the channel that will be monitored for forwarding messages
+ CHANNEL_TO_TRACK = #ID of the channel to which the posts will be sent
